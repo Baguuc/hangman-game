cd03e8dd0e947954e0eaf55c9515a7bd48fa988d
Jan Żukowski <janzukowski@proton.me>
2024-09-22 17:02:16 +0200

    update: added Game class

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿class Game
+{
+    string[] words;
+    string correctWord;
+    HashSet<char> correctWords;
+    HashSet<char> wrongWords;
+
+    public Game(string wordListPath)
+    {
+        this.words = File.ReadAllText(wordListPath).Split('\n');
+        this.correctWords = new HashSet<char>();
+        this.wrongWords = new HashSet<char>();
+
+        Random random = new Random();
+        int randomWordIndex = random.Next(0, words.Length - 1);
+        string randomWord = words[randomWordIndex];
+
+        this.correctWord = randomWord;
+    }
+}
